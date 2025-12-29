a88c4187a4ae1652e55d63e8ca711f08e8a2b042
amanrai00 <amanrai1630@gmail.com>
2025-12-30 00:28:01 +0900

    trail

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -127,7 +127,7 @@
               </button>
             </li>
           </ul>
-
+asdasdasd
           <a
             href=""
             class="group relative inline-block w-max px-4 py-2 font-semibold"
