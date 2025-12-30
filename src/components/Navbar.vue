8f15fb6c4a0623fa572e96b537baaa7c0695425e
amanrai00 <amanrai1630@gmail.com>
2025-12-31 00:43:28 +0900

    navbar concept One

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -120,7 +120,7 @@
       <Transition name="mobile-slide">
         <div
           v-if="isOpen"
-          class="absolute left-0 top-full w-full bg-[#404040] text-white lg:hidden font-semibold overflow-hidden origin-top"
+          class="absolute left-0 top-full w-full bg-[#404040] text-white lg:hidden font-semibold overflow-hidden origin-top px-10"
         >
           <div class="flex flex-col gap-4 px-5 pb-5 pt-4">
             <ul class="flex flex-col gap-4">
@@ -128,8 +128,14 @@
                 v-for="item in sections"
                 :key="item.id"
               >
-                <button @click="scrollToSection(item.id)">
-                  {{ item.label }}
+                <button @click="scrollToSection(item.id)"
+                class="group relative inline-flex flex-col items-start font-semibold transition-transform duration-150 hover:scale-[1.08]"
+                >
+                <span>{{ item.label }}</span>
+
+                <span class="mt-1 block w-full bg-white/30 transition-transform duration-300 group-hover:scale-x-110 group-hover:scale-y-[1.3]"
+                style="height: 0.5px"
+                ></span>
                 </button>
               </li>
             </ul>
